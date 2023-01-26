4f4efee77f222e06821cea0a9613e62b89363124
CMD-God <48065803+CMD-God@users.noreply.github.com>
2023-01-26 18:38:51 +0200

    Initial Upload

---
+++ src/main/java/com/cmdgod/mc/cmd_origins/CmdOriginsMod.java
@@ -0,0 +1,13 @@
+package com.cmdgod.mc.cmd_origins;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CmdOriginsMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
